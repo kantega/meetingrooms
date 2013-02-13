ec24a5dd5b70fe6b4556b1859e02065d3b933702
nturpin <nadia.rincon.turpin@kantega.no>
2013-02-13 16:25:09 +0100

    Litt mer refactoring.

---
+++ ios/kantoko/kantoko/view/BookingButton.h
@@ -0,0 +1,13 @@
+//
+//  BookingButton.h
+//  kantoko
+//
+//  Created by Nadia Turpin on 2/13/13.
+//  Copyright (c) 2013 Kantega. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BookingButton : UIButton
+
+@end
---
+++ ios/kantoko/kantoko/view/BookingButton.m
@@ -0,0 +1,31 @@
+//
+//  BookingButton.m
+//  kantoko
+//
+//  Created by Nadia Turpin on 2/13/13.
+//  Copyright (c) 2013 Kantega. All rights reserved.
+//
+
+#import "BookingButton.h"
+
+@implementation BookingButton
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
